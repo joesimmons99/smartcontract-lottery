8e104f2c73e7d73436ec44e9eaf8f0f42f6af8a5
“Joe <joe_simmons_99@yahoo.com>
2022-01-23 09:10:20 -0800

    added enter_lottery function. runs successfully!

--- scripts/deploy_lottery.py
+++ scripts/deploy_lottery.py
@@ -22,6 +22,15 @@ def start_lottery():
     starting_tx.wait(1) # Wait for the tx to be mined. If you don't include this, you may get an error.
     print("Started lottery!")
 
+def enter_lottery():
+    account = get_account()
+    lottery = Lottery[-1]
+    value = lottery.getEntranceFee() + 100000000
+    tx = lottery.enter({"from": account, "value": value})
+    tx.wait(1)
+    print("You entered the lottery!")
+
 def main():
     deploy_lottery()
     start_lottery()
+    enter_lottery()
