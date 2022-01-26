ab4beae7a0a64508907d4c84e55ad2411bd5c779
“Joe <joe_simmons_99@yahoo.com>
2022-01-25 16:31:38 -0800

    added unit test and successfully tested test_get_entrance_fee

--- tests/test_lottery_unit.py
+++ tests/test_lottery_unit.py
@@ -2,18 +2,16 @@
 from brownie import Lottery, accounts, config, network
 from scripts.deploy_lottery import deploy_lottery
 from web3 import Web3
-import pytest
+# import pytest
 
 def test_get_entrance_fee():
-        if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
-            pytest.skip()
         # Arrange
         lottery = deploy_lottery()
         # Act
-        # 2000 eth / usd
+        # 2,000 eth / usd
         # usdEntryFee = 50
-        # entryFee = 2000 / usdEntryFee
-        expected_entry_fee = Web3.toWei(0.025, "ether")
+        # 2000/1 == 50/x == 0.025
+        expected_entrance_fee = Web3.toWei(0.025, "ether")
         entrance_fee = lottery.getEntranceFee()
         # Assert
-        assert expected_entry_fee == entrance_fee
+        assert expected_entrance_fee == entrance_fee
