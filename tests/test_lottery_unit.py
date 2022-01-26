0f7b5d33e2b57e3e83d49b09448ecc61b2cb8c52
“Joe <joe_simmons_99@yahoo.com>
2022-01-25 21:20:20 -0800

    added and passed two more unit tests

--- tests/test_lottery_unit.py
+++ tests/test_lottery_unit.py
@@ -1,10 +1,16 @@
 
-from brownie import Lottery, accounts, config, network
+from scripts.helpful_scripts import LOCAL_BLOCKCHAIN_ENVIRONMENTS, get_account
+from brownie import Lottery, accounts, config, network, exceptions
 from scripts.deploy_lottery import deploy_lottery
 from web3 import Web3
+import pytest
+
+from scripts.helpful_scripts import LOCAL_BLOCKCHAIN_ENVIRONMENTS
 # import pytest
 
 def test_get_entrance_fee():
+        if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+                pytest.skip("This test is only for local blockchain environments")
         # Arrange
         lottery = deploy_lottery()
         # Act
@@ -15,3 +21,19 @@ def test_get_entrance_fee():
         entrance_fee = lottery.getEntranceFee()
         # Assert
         assert expected_entrance_fee == entrance_fee
+
+def test_cant_enter_unless_started():
+        if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+                pytest.skip()
+        lottery = deploy_lottery()
+        with pytest.raises(exceptions.VirtualMachineError):
+                lottery.enter({"from": get_account(), "value": lottery.getEntranceFee()})
+
+def test_can_start_and_enter_lottery():
+        if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+                pytest.skip()
+        lottery = deploy_lottery()
+        account = get_account()
+        lottery.startLottery({"from": account})
+        lottery.enter({"from": account, "value": lottery.getEntranceFee()})
+        assert lottery.players(0) == account
